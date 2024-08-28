30a30ee166acbd9443fe8d0270e8d09ec86604ea
JoseLinoDC <joselinodiaz25@gmail.com>
2024-08-28 13:24:41 -0700

    Se añadieron algunos ejercicios xd

---
+++ Ejercicios/ConteoCaracteres.js
@@ -0,0 +1,10 @@
+// Función para contar los caracteres de una cadena
+function contarCaracteres(cadena) {
+    return cadena.length;
+}
+
+// Ejemplo de uso
+const texto = "Hola, ¿cómo estás?";
+const numeroDeCaracteres = contarCaracteres(texto);
+
+console.log("El número de caracteres en la cadena es: " + numeroDeCaracteres);
